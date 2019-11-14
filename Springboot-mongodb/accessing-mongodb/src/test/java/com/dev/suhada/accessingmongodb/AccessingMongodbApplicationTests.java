3a3c0567430dae12a3abeaf6cf7c0d832cbff52b
dev-tobil <ade.suhada@mail.ugm.ac.id>
2019-11-14 16:34:35 +0700

    add spring-boot mongodb

---
+++ Springboot-mongodb/accessing-mongodb/src/test/java/com/dev/suhada/accessingmongodb/AccessingMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.suhada.accessingmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
